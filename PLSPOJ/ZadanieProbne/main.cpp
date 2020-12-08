dc105988013045401e2cfa470daaab59b676df6b
NightLunaStudio <NightLuna.Studio@gmail.com>
2020-12-08 13:28:31 +0000

    My first solved problem on SPOJ

---
+++ PLSPOJ/ZadanieProbne/main.cpp
@@ -0,0 +1,17 @@
+/*
+	Napisac program do dodawania dwoch liczb calkowitych. Na wejsciu podane sa w oddzielnych liniach dwie liczby naturalne A oraz B mniejsze od 200. 
+	Na wyjsciu nalezy wypisac wartosc ich sumy, A + B.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+
+	cin >> a >> b;
+	cout << a + b;
+
+	return 0;
+}
